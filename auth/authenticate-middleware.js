27d9ee891dceb63f4e32000ff9adf502b4884eff
Afia-Lydia <you@example.com>
2019-12-20 12:08:24 -0500

    was able to get registration and sign in working, running into a problem with the restricted get request.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,26 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+	const { authenticate } = req.headers;
+
+	if (authenticate) {
+		const secret =
+			process.env.JWT_SECRET || "FLCL is not rewatchable. Fight me.";
+
+		jwt.verify(authenticate, secret, function(err, decodedToken) {
+			if (err) {
+				res
+					.status(401)
+					.json({
+						message: "Invalid Token - unfortunately you shall not pass!"
+					});
+			} else {
+				req.token = decodedToken;
+
+				next();
+			}
+		});
+	} else {
+		res.status(400).json({ message: "Please login and try again" });
+	}
 };
